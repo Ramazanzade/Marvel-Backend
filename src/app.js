832056803dd503df64c08f0d8056676450a61a10
Nizam <ramazanov570633@gmail.com>
2023-06-25 01:43:19 +0400

    netlify

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const app = express()
 require('dotenv').config();
 const UserRouter = require('./api/routers/userrouter')
 const fileUploadrouter = require('./api/routers/fileuploadrouter')
+const serverless = require("serverless-http");
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
@@ -15,14 +16,13 @@ mongoose.connect(CONNECTION_STRING)
 app.options("*", cors({ origin: ['http://localhost:19006',  'http://localhost:8080','https://fluffy-tarsier-c3f7df.netlify.app', "https://marvel-backend-vzsr.onrender.com"], optionsSuccessStatus: 200 }));
 app.options("*", cors({ origin: '*', optionsSuccessStatus: 200 }));
 app.use('/api/user',UserRouter)
-app.use('/api/file',fileUploadrouter)
+app.use('/.netlify/functions/api/file',fileUploadrouter)
 app.use((err,res)=>{
     res.status(err.statusCode || 500).json({
         message:err?.message || "Server error",
         statusCode:err.statusCode || 500
     })
 })
-// fileUploadrouter.arguments
-// yarn cache clean
-// kslsd;k,csklds;ckusjdfkvhddkfv
+
 module.exports=app
+module.exports.handler = serverless(app);
