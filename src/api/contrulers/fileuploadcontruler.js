2e1f363af0a0aae98f676d82448266dc872a4a7f
Nizam <ramazanov570633@gmail.com>
2023-06-24 15:51:01 +0400

    File limit7

--- src/api/contrulers/fileuploadcontruler.js
+++ src/api/contrulers/fileuploadcontruler.js
@@ -1,26 +1,28 @@
 require('dotenv').config();
 const fileUpload = require('express-fileupload');
-const path =require('path')
-exports.sendfileupload= async (req,res)=>{
- res.sendFile(path.join(__dirname,'http://localhost:19006'))
-}
+const path = require('path');
+exports.sendfileupload = async (req, res) => {
+  res.sendFile('/path/to/file.jpg');
+};
 
-exports.creatfileupload = async(req,res)=>{
-    try {
-        if (!req.files || Object.keys(req.files).length === 0) {
-          return res.status(400).json({ message: 'No files were .' });
-        }
-        const file = req.files.file;
-        console.log(file);
-    Object.keys(file).forEach(key =>{
-      const filepaht = path.json(__dirname, 'file', file[key].name)
-      file[key].mv(filepaht, (err)=>{
-        if(err) return res.status(500).json({status:'error', message:err})
-      })
-    })
-        return res.json({ status: 'success', message:Object.keys(file).toString() });
-      } catch (err) {
-        console.log('Error uploading file:', err);
-        return res.status(500).json({ message: 'Error uploading file.' });
-      }
-}
+
+exports.creatfileupload = async (req, res) => {
+  try {
+    if (!req.files || Object.keys(req.files).length === 0) {
+      return res.status(400).json({ message: 'No files were uploaded.' });
+    }
+
+    for (let key in req.files) {
+      const file = req.files[key];
+      const filePath = path.join(__dirname, 'file', file.name);
+      file.mv(filePath, (err) => {
+        if (err) return res.status(500).json({ status: 'error', message: err });
+      });
+    }
+
+    return res.json({ status: 'success', message: Object.keys(req.files).toString() });
+  } catch (err) {
+    console.log('Error uploading file:', err);
+    return res.status(500).json({ message: 'Error uploading file.' });
+  }
+};
