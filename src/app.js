5d4de90d237ce4a1ee16301928ca9c0c2181779d
Nizam <ramazanov570633@gmail.com>
2023-06-25 23:40:44 +0400

    netlify9

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true }))
 mongoose.connect(CONNECTION_STRING)                                                     
 .then(res=>console.log('connect'))
 .catch(err=>console.log(err))
-app.options("*", cors({ origin: ['http://localhost:19006',  'http://localhost:8080','https://fluffy-tarsier-c3f7df.netlify.app', "https://marvel-backend2.onrender.com"], optionsSuccessStatus: 200 }));
+app.options("*", cors({ origin: ['http://localhost:19006',  'http://localhost:8080', "https://marvel-backend2.onrender.com"], optionsSuccessStatus: 200 }));
 app.options("*", cors({ origin: '*', optionsSuccessStatus: 200 }));
 app.use('/.netlify/functions/api/user',UserRouter)
 app.use('/.netlify/functions/api/file',fileUploadrouter)
