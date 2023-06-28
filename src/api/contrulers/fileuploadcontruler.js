b64010995b0ef7187e6fb4b4b895f5b7afa63387
Nizam <ramazanov570633@gmail.com>
2023-06-29 03:25:35 +0400

    File upload 26

--- src/api/contrulers/fileuploadcontruler.js
+++ src/api/contrulers/fileuploadcontruler.js
@@ -18,8 +18,7 @@ const storage = multer.diskStorage({
   },
   filename: (req, file, cb) => {
     const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1E9)}`;
-    const fileExtension = path.extname(file.originalname);
-    cb(null, `${file.originalname}-${uniqueSuffix}${fileExtension}`);
+    cb(null, `${file.originalname}-${uniqueSuffix}`);
   },
 });
 
