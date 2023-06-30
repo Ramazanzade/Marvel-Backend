986ef9a421598252efdeeb091eb76dbf700fc687
Nizam <ramazanov570633@gmail.com>
2023-06-30 17:39:42 +0400

    File upload 57

--- src/api/routers/fileuploadrouter.js
+++ src/api/routers/fileuploadrouter.js
@@ -12,7 +12,7 @@ const fileexlimiter = require("../../Middlewares/fileextlimiter")
 const filePayload =require('../../Middlewares/filePayload')
 const FileSizeLimit=require('../../Middlewares/fileSizeLimit')
 
-router.get('/', filesget);
+router.get('/:id', filesget);
 router.post('/file',fileadd );
 router.delete('/file/:id',filedelet);
 router.put('/file/:id',fileupdate)
