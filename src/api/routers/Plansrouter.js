ef3f30b8bb530f6aa6474c90fdaaa16b35556451
Nizam <ramazanov570633@gmail.com>
2023-07-08 04:27:55 +0400

    Plans4

--- src/api/routers/Plansrouter.js
+++ src/api/routers/Plansrouter.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { Planscountreler } = require('../contrulers/Planscountreler');
 
 router.post('/plans', Planscountreler.add)
-router.get('/plans', Planscountreler.getAll)
+router.get('/', Planscountreler.getAll)
 router.get('/plans/:id', Planscountreler.update)
 router.delete('/plans/:id', Planscountreler.delete)
 router.put('/plans/:id', Planscountreler.put);
