47f1e7e200915a47ea598dc3337d1d6e61f6671f
Nizam <ramazanov570633@gmail.com>
2023-07-09 02:00:50 +0400

    Peyment2

--- src/app.js
+++ src/app.js
@@ -23,6 +23,43 @@ app.use('/api/file',fileUploadrouter)
 app.use('/api/splas', splasfilerouter)
 app.use('/api/onboarding',onboardinfilerouter)
 app.use('/api/plans', Plansrouter)
+const stripe = require('stripe')('pk_test_51NRj63FWhGiOytmS6448C37rHae30uwfgDnx6vKieLjrxVOPRaT8scx54QyKQbAbrhBLcJpZjLoWdc9zAImVc2p300qtQxsrwh');
+app.post('/api/pay', async (req, res) => {
+    const { name, surname, cart, date, security } = req.body;
+  
+    try {
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: 1000, 
+        currency: 'usd',
+        payment_method_types: ['card'],
+        description: `Payment from ${name} ${surname}`,
+      });
+  
+  
+      res.status(200).json({ clientSecret: paymentIntent.client_secret });
+    } catch (error) {
+      console.error('Error processing payment:', error);
+      res.status(500).json({ error: 'Payment processing failed' });
+    }
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 app.use((err,res)=>{
     res.status(err.statusCode || 500).json({
         message:err?.message || "Server error",
